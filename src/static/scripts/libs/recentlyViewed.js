811cfbc5054e4ae1d8940eed562475ff92f36c88
Buz Carter <buzcarter@gmail.com>
2023-12-10 16:32:52 -0800

    recents as a modal

--- src/static/scripts/libs/recentlyViewed.js
+++ src/static/scripts/libs/recentlyViewed.js
@@ -5,7 +5,19 @@ const MAX_LIST_LENGTH = 50;
 const Selectors = {
   RECIPE_LIST: '#recipe-list',
   RECENTLY_VIEWED_BTN: '#show-recents-btn',
+
+  MODAL: '#show-recents-modal',
+  MODAL_CONTENT: '#show-recents-content',
+  MODAL_CLOST_BTN: '#show-recents-close-btn',
 };
+
+const Styles = {
+  MODAL_ACTIVE: 'modal--is-active',
+};
+
+const toDisplay = (value) => value
+  .replace(/-/g, ' ')
+  .replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
 
 function onRecipeLinkClick(e) {
   const anchor = e.target.tagName === 'A'
@@ -20,13 +32,22 @@ function onRecipeLinkClick(e) {
   }
 }
 
+function toggleModal(isActive) {
+  const modal = document.querySelector(Selectors.MODAL);
+  modal.classList.toggle(Styles.MODAL_ACTIVE, isActive);
+  modal.setAttribute('aria-hidden', !isActive);
+}
+
 function onViewBtnClick() {
-  //
-  alert(getKey(KeyNames.RECENT));
+  toggleModal(true);
+  document.querySelector(Selectors.MODAL_CONTENT).innerHTML = getKey(KeyNames.RECENT)
+    .map((value) => `<li><a href="${value}.html">${toDisplay(value)}</a></li>`)
+    .join('');
 }
 
 export function init() {
   document.querySelector(Selectors.RECENTLY_VIEWED_BTN).addEventListener('click', onViewBtnClick);
   // event delegation: record all recipe link clicks
   document.querySelector(Selectors.RECIPE_LIST).addEventListener('click', onRecipeLinkClick);
+  document.querySelector(Selectors.MODAL_CLOST_BTN).addEventListener('click', () => toggleModal(false));
 }
